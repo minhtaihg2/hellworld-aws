d292d505ca0d763d88e21da33aeca8d7e8adbf59
Phạm Minh Tài <taipham.it@gmail.com>
2018-11-26 10:01:04 +0700

    Update info.php

--- info.php
+++ info.php
@@ -1,4 +1,5 @@
 <?php
 echo '<h1 style="margin-top:100px;text-align:center;margin-bottom:100px;">Welcome AWS!!!</h1>';
+echo '<h2 style="margion-top:15px;text-align:center">Version 0.1</h2>';
 echo phpinfo();
 ?>
